38c13ca02124448669e4e07d6c7b5a795b90b4d4
Sara Blom <sara.cecilia.blom@gmail.com>
2022-10-14 15:57:17 +0200

    move delete button to separate component

--- src/components/gallery/GalleryItem.js
+++ src/components/gallery/GalleryItem.js
@@ -1,19 +1,9 @@
-import { useContext } from "react";
-import { Context } from "../../context/Context";
-import { MdDeleteForever, MdDownload, MdAccessTime } from "react-icons/md";
+import { MdDownload, MdAccessTime } from "react-icons/md";
 import { RiMapPinLine } from "react-icons/ri";
-import { handleDeletePhoto } from "../../utils/cameraHelpers";
+
+import DeleteButton from "../buttons/DeleteButton";
 
 function GalleryItem({ item }) {
-    const [context, updateContext] = useContext(Context);
-
-    const onClickHandler = () => {
-        const newGallery = handleDeletePhoto(item.id, context.gallery);
-        updateContext({
-            gallery: newGallery,
-        });
-    };
-
     return (
         <li className="img-item">
             <img src={item.src} alt="Gallery item" className="picture-taken" />
@@ -25,13 +15,7 @@ function GalleryItem({ item }) {
                     <RiMapPinLine /> {item.city}, {item.country}
                 </p>
             )}
-            <button
-                aria-label="Delete this photo"
-                className="btn"
-                onClick={onClickHandler}
-            >
-                <MdDeleteForever className="small-icon" />
-            </button>
+            <DeleteButton item={item} />
 
             <a alt="Download this picture" href={item.src} download>
                 <button className="btn" aria-label="Download this photo">
