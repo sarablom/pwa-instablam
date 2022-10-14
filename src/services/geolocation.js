619dd39f6ce608e6704f8ff3707d9935bd507eea
Sara Blom <sara.cecilia.blom@gmail.com>
2022-10-14 15:57:29 +0200

    change return value

--- src/services/geolocation.js
+++ src/services/geolocation.js
@@ -1,14 +1,14 @@
 export async function fetchDataByUrl(url) {
-	try {
-		const response = await fetch(url);
-		return await response.json();
-	} catch (err) {
-		console.log(err);
-	}
+    try {
+        const response = await fetch(url);
+        return await response.json();
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export async function getLocation(latitude, longitude) {
-  const fetchURL = `https://geocode.xyz/${latitude},${longitude}?geoit=json&auth=304512200627251254779x59553`
-  const data = await fetchDataByUrl(fetchURL);
-  return data;
+    const fetchURL = `https://geocode.xyz/${latitude},${longitude}?geoit=json&auth=304512200627251254779x59553`;
+    const data = await fetchDataByUrl(fetchURL);
+    return { city: data.city, country: data.country, error: data.error };
 }
