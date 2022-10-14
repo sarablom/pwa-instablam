96bcdfc31c7e786fa23c6b0dec4d5f0cca09eef9
Sara Blom <sara.cecilia.blom@gmail.com>
2022-10-14 15:56:27 +0200

    add a delete button

---
+++ src/components/buttons/DeleteButton.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { Context } from "../../context/Context";
+import { handleDeletePhoto } from "../../utils/cameraHelpers";
+import { MdDeleteForever } from "react-icons/md";
+
+function DeleteButton({ item }) {
+    const [context, updateContext] = useContext(Context);
+
+    const onClickHandler = () => {
+        const newGallery = handleDeletePhoto(item.id, context.gallery);
+        localStorage.setItem("gallery", JSON.stringify(newGallery));
+        updateContext({
+            gallery: newGallery,
+        });
+    };
+    return (
+        <button
+            aria-label="Delete this photo"
+            className="btn"
+            onClick={onClickHandler}
+        >
+            <MdDeleteForever className="small-icon" />
+        </button>
+    );
+}
+
+export default DeleteButton;
