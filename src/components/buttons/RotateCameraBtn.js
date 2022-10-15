5dd9faf14d8b255560fca532ed6f0c21b144a135
Sara Blom <sara.cecilia.blom@gmail.com>
2022-10-15 22:40:26 +0200

    refactor onclick function

--- src/components/buttons/RotateCameraBtn.js
+++ src/components/buttons/RotateCameraBtn.js
@@ -1,20 +1,31 @@
 import { RiCameraSwitchFill } from "react-icons/ri";
+import { turnCameraOn } from "../../utils/cameraHelpers";
 
-export default function RotateCameraBtn({ facing, setFacing, turnCameraOff, handleTurnCameraOn }) {
-
-  function changeFacing() {
-    if (facing === "user") {
-      setFacing("environment");
-    } else {
-      setFacing("user");
+export default function RotateCameraBtn({
+    facing,
+    setFacing,
+    turnCameraOff,
+    video,
+    setStream,
+}) {
+    async function changeFacing() {
+        if (facing === "user") {
+            setFacing("environment");
+        } else {
+            setFacing("user");
+        }
+        turnCameraOff();
+        const currentStream = await turnCameraOn(video, facing);
+        setStream(currentStream);
     }
-    turnCameraOff();
-    handleTurnCameraOn();
-  }
 
-  return (
-    <button className="btn" aria-label="Rotate camera" onClick={changeFacing}>
-      <RiCameraSwitchFill className="icon" />
-    </button>
-  );
+    return (
+        <button
+            className="btn"
+            aria-label="Rotate camera"
+            onClick={changeFacing}
+        >
+            <RiCameraSwitchFill className="icon" />
+        </button>
+    );
 }
