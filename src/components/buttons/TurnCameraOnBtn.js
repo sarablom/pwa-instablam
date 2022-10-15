8f385e8e9455d325ec6be7fa3f6c79f1596e559c
Sara Blom <sara.cecilia.blom@gmail.com>
2022-10-15 22:41:15 +0200

    refactor onclick function

--- src/components/buttons/TurnCameraOnBtn.js
+++ src/components/buttons/TurnCameraOnBtn.js
@@ -2,15 +2,17 @@ import React from "react";
 import { turnCameraOn } from "../../utils/cameraHelpers";
 import { FaPlay } from "react-icons/fa";
 
-const TurnCameraOnBtn = ({ video, facing, setCameraOn }) => {
+const TurnCameraOnBtn = ({ video, facing, setCameraOn, setStream }) => {
+    const handleTurnCameraOn = async () => {
+        const currentStream = await turnCameraOn(video, facing);
+        setCameraOn(true);
+        setStream(currentStream);
+    };
     return (
         <button
             className="btn"
             aria-label="Turn camera on"
-            onClick={() => {
-                turnCameraOn(video, facing);
-                setCameraOn(true);
-            }}
+            onClick={handleTurnCameraOn}
         >
             <FaPlay className="icon" />
         </button>
