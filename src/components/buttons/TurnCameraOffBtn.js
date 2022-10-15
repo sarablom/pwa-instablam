9859afa9743bd602a036dc03dd66e48eb5828f76
Sara Blom <sara.cecilia.blom@gmail.com>
2022-10-15 22:41:00 +0200

    lint fix

--- src/components/buttons/TurnCameraOffBtn.js
+++ src/components/buttons/TurnCameraOffBtn.js
@@ -1,10 +1,13 @@
 import { RiCameraOffFill } from "react-icons/ri";
 
 export default function TurnCameraOffBtn({ turnCameraOff }) {
-
-  return (
-    <button className="btn" aria-label="Turn camera off" onClick={turnCameraOff}>
-      <RiCameraOffFill className="icon" />
-    </button>
-  );
+    return (
+        <button
+            className="btn"
+            aria-label="Turn camera off"
+            onClick={turnCameraOff}
+        >
+            <RiCameraOffFill className="icon" />
+        </button>
+    );
 }
